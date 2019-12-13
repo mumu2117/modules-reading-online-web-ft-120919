a13ce200d08e50f9c2e201277728744aa196039c
Jennifer Miller <jmiller@archersystems.us>
2019-12-13 13:31:34 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
-require_relative './dance_module.rb' 	
+
 
 class Dancer
   extend MetaDancing
