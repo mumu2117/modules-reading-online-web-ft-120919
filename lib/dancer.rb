e0d241ebb1d49a4962117d0f3f17c58e2d61adf6
Jennifer Miller <jmiller@archersystems.us>
2019-12-13 13:31:19 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,7 +1,7 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
-require_relative './class_methods_module.rb'	
+
 require_relative './dance_module.rb' 	
 
 class Dancer
